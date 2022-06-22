cac2c9771e1f4ad22c1c2e9e6b28e684e655330f
zhangshoukun <test@runoob.com>
2022-06-22 17:02:15 +0800

    增加VFXLab特效库

---
+++ SKFramework/Editor/VFXLab/VFXInfo.cs
@@ -0,0 +1,41 @@
+﻿namespace SK.Framework
+{
+    /// <summary>
+    /// 特效信息
+    /// </summary>
+    public class VFXInfo
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string name;
+        /// <summary>
+        /// 作者
+        /// </summary>
+        public string author;
+        /// <summary>
+        /// 发布日期
+        /// </summary>
+        public string releaseDate;
+        /// <summary>
+        /// 介绍
+        /// </summary>
+        public string description;
+        /// <summary>
+        /// 开发环境
+        /// </summary>
+        public string ide;
+        /// <summary>
+        /// 所在分组
+        /// </summary>
+        public string group;
+        /// <summary>
+        /// 文档地址
+        /// </summary>
+        public string documentationUrl;
+        /// <summary>
+        /// 用于查看的链接
+        /// </summary>
+        public string viewLink;
+    }
+}
